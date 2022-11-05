2924f5a27aaca65e30c37f9fcd1d1e7dbe45fdd6
bledda <louchebem06@gmail.com>
2022-11-05 07:30:15 +0100

    UPDATE: add call action

--- app/src/main/java/com/school42/ft_hangouts/ContactAdapter.java
+++ app/src/main/java/com/school42/ft_hangouts/ContactAdapter.java
@@ -1,7 +1,13 @@
 package com.school42.ft_hangouts;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +15,15 @@ import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.core.app.ActivityCompat;
+
 import java.util.Vector;
 
 public class ContactAdapter extends BaseAdapter {
 
-	private Context _context;
-	private Vector<Contact> _vectorContact;
-	private LayoutInflater _inflater;
-	private Contact _currentContact;
+	private final Context _context;
+	private final Vector<Contact> _vectorContact;
+	private final LayoutInflater _inflater;
 
 	ContactAdapter(Context context, Vector<Contact> contacts) {
 		_context = context;
@@ -38,8 +45,8 @@ public class ContactAdapter extends BaseAdapter {
 	public View getView(int i, View view, ViewGroup viewGroup) {
 		view = _inflater.inflate(R.layout.adapter_contact, null);
 
-		_currentContact = getItem(i);
-		String fullname = !_currentContact.getSurname().isEmpty() ? _currentContact.getSurname() : _currentContact.getFullName();
+		Contact current = getItem(i);
+		String fullname = !current.getSurname().isEmpty() ? current.getSurname() : current.getFullName();
 
 		TextView fullnameText = view.findViewById(R.id.fullname);
 		fullnameText.setText(fullname);
@@ -49,40 +56,51 @@ public class ContactAdapter extends BaseAdapter {
 		ImageButton call = view.findViewById(R.id.call);
 		ImageButton edit = view.findViewById(R.id.edit);
 
-		delete.setOnClickListener(new DeleteListener(_currentContact));
+		delete.setOnClickListener(new DeleteListener(current));
 		msg.setOnClickListener(new MsgListener());
-		call.setOnClickListener(new CallListener());
+		call.setOnClickListener(new CallListener(current));
 		edit.setOnClickListener(new EditListener());
 
 		return (view);
 	}
 
 	class DeleteListener implements View.OnClickListener {
-		private Contact current;
+		private final Contact current;
 
 		DeleteListener(Contact r) { current = r; }
+
 		@Override
 		public void onClick(View view) {
-			if (!current.delete(_context))
-				return ;
+			current.delete(_context);
 		}
 	}
 
 	class CallListener implements View.OnClickListener {
+		private final Contact current;
+
+		CallListener(Contact r) { current = r; }
+
+		@Override
+		public void onClick(View view) {
+			String phone = current.getPhone();
+			Intent call = new Intent(Intent.ACTION_CALL);
+			call.setData(Uri.parse("tel:" + phone));
+			try {
+				_context.startActivity(call);
+			} catch (Exception e) {
+				ActivityCompat.requestPermissions((Activity) _context,new String[]{Manifest.permission.CALL_PHONE}, 1);
+			}
+		}
+	}
+
+	static class EditListener implements View.OnClickListener {
 		@Override
 		public void onClick(View view) {
 
 		}
 	}
 
-	class EditListener implements View.OnClickListener {
-		@Override
-		public void onClick(View view) {
-
-		}
-	}
-
-	class MsgListener implements View.OnClickListener {
+	static class MsgListener implements View.OnClickListener {
 		@Override
 		public void onClick(View view) {
 
